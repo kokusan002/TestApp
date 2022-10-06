583a6465f8285ec88fc17e2f4f4a2d2309e5b5c4
kokusan <hoppe292@icloud.com>
2022-10-06 21:16:03 +0900

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by 久澄慎吾 on 2022/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
